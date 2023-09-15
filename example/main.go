0b6519eae595e4d2f687f4e2af27802cee5be709
ngicks <yknt.bsl@gmail.com>
2023-09-15 00:02:22 +0000

    👷: example follow change

--- example/main.go
+++ example/main.go
@@ -3,69 +3,67 @@ package main
 import (
 	"context"
 	"fmt"
+	"io"
 	"os"
 
 	"github.com/compose-spec/compose-go/loader"
 	"github.com/compose-spec/compose-go/types"
 	"github.com/docker/cli/cli/command"
 	"github.com/docker/compose/v2/pkg/api"
-	"github.com/docker/docker/client"
 	"github.com/ngicks/compose-wrapper/compose"
 )
 
 func main() {
 	projectName := "testdata"
-	project, err := loader.LoadWithContext(
-		context.Background(), types.ConfigDetails{
-			WorkingDir: "./testdata",
+	loader, err := compose.NewLoader(
+		projectName,
+		types.ConfigDetails{
+			WorkingDir: "../compose/testdata",
 			ConfigFiles: []types.ConfigFile{
-				{Filename: "./testdata/compose.yml"},
-				{Filename: "./testdata/additional.yml"},
+				{Filename: "../compose/testdata/compose.yml"},
+				{Filename: "../compose/testdata/additional.yml"},
 			},
 			Environment: types.NewMapping(os.Environ()),
 		},
-		func(o *loader.Options) {
-			o.SetProjectName(projectName, true)
-		},
-	)
-	if err != nil {
-		panic(err)
-	}
-
-	dockerClient, err := client.NewClientWithOpts(client.WithAPIVersionNegotiation())
-	if err != nil {
-		panic(err)
-	}
-	if err := client.FromEnv(dockerClient); err != nil {
-		panic(err)
-	}
-
-	service, err := compose.NewComposeService(
-		projectName,
-		project,
+		[](func(*loader.Options)){},
 		nil,
-		os.Stdout, os.Stderr,
-		command.WithAPIClient(dockerClient),
+		command.WithOutputStream(io.Discard),
+		command.WithErrorStream(io.Discard),
 	)
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println("compose service created")
-	list, err := service.Ps(context.Background(), api.PsOptions{Project: project, All: true})
+
+	service, err := loader.LoadComposeService(context.Background())
 	if err != nil {
 		panic(err)
 	}
-	fmt.Printf("%+#v\n", list)
-	if _, err := service.Create(context.Background(), api.CreateOptions{
+
+	fmt.Println("compose service created")
+	list, err := service.Ps(context.Background(), api.PsOptions{All: true})
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%+#v\n\n", list)
+	out, err := service.Create(context.Background(), api.CreateOptions{
 		Recreate:             api.RecreateDiverged,
 		RecreateDependencies: api.RecreateDiverged,
 		Inherit:              true,
-	}); err != nil {
-		panic(err)
-	}
-	list, err = service.Ps(context.Background(), api.PsOptions{Project: project, All: true})
+	})
 	if err != nil {
 		panic(err)
 	}
-	fmt.Printf("%+#v\n", list)
+	fmt.Printf("%+#v\n\n", out)
+
+	list, err = service.Ps(context.Background(), api.PsOptions{All: true})
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%+#v\n\n", list)
+
+	exitCode, stdout, stderr, err := service.RunOneOffContainer(context.Background(), api.RunOptions{Service: "additional"})
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("exitCode = %d, stdout = %s, stderr = %s\n\n", exitCode, stdout, stderr)
 }
