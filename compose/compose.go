1ed7e885584d1abdaaedc5ecab94d830a4eaa2fc
ngicks <yknt.bsl@gmail.com>
2023-09-19 00:11:03 +0000

    🐛: fix outputs are reset to stdout/stderr when entering dry run mode

--- compose/compose.go
+++ compose/compose.go
@@ -3,14 +3,15 @@ package compose
 import (
 	"bytes"
 	"context"
-	"io"
 	"strings"
 	"sync"
 
 	"github.com/compose-spec/compose-go/types"
 	"github.com/docker/cli/cli/command"
+	"github.com/docker/cli/cli/flags"
 	"github.com/docker/compose/v2/pkg/api"
 	"github.com/docker/compose/v2/pkg/compose"
+	"github.com/docker/docker/client"
 )
 
 // AddDockerComposeLabel changes service.CustomLabels so that is can be found by docker compose v2.
@@ -33,6 +34,8 @@ func AddDockerComposeLabel(project *types.Project) {
 type ComposeService struct {
 	mu          sync.Mutex
 	out, err    *bytes.Buffer
+	dryRun      bool
+	cli         *command.DockerCli
 	projectName string
 	project     *types.Project
 	service     api.Service
@@ -49,34 +52,28 @@ func NewComposeService(
 
 	var bufOut, bufErr = new(bytes.Buffer), new(bytes.Buffer)
 
-	if out := dockerCli.Out(); out != nil {
-		w := io.MultiWriter(bufOut, out)
-		_ = dockerCli.Apply(command.WithOutputStream(w))
-	} else {
-		_ = dockerCli.Apply(command.WithOutputStream(bufOut))
-	}
-
-	if err := dockerCli.Err(); err != nil {
-		w := io.MultiWriter(bufErr, err)
-		_ = dockerCli.Apply(command.WithErrorStream(w))
-	} else {
-		_ = dockerCli.Apply(command.WithErrorStream(bufErr))
-	}
-
 	serviceProxy := api.NewServiceProxy().WithService(compose.NewComposeService(dockerCli))
 
-	return &ComposeService{
+	s := &ComposeService{
 		out:         bufOut,
 		err:         bufErr,
+		cli:         dockerCli,
+		dryRun:      false,
 		service:     serviceProxy,
 		projectName: projectName,
 		project:     project,
 	}
+	s.overrideOutputStreams()
+	return s
 }
 
-func (c *ComposeService) resetBuf() {
-	c.out.Reset()
-	c.err.Reset()
+func (s *ComposeService) overrideOutputStreams() {
+	_ = s.cli.Apply(command.WithOutputStream(s.out), command.WithErrorStream(s.err))
+}
+
+func (s *ComposeService) resetBuf() {
+	s.out.Reset()
+	s.err.Reset()
 }
 
 func (c *ComposeService) parseOutput() ComposeOutput {
@@ -212,8 +209,31 @@ func (c *ComposeService) Remove(ctx context.Context, options api.RemoveOptions) 
 // DryRunMode switches c to dry run mode if dryRun is true.
 // Implementations might not change back to normal mode even if dryRun is false.
 // User must call this only once and only when the user whishes to use dry run client.
-func (c *ComposeService) DryRunMode(ctx context.Context, dryRun bool) (context.Context, error) {
-	c.mu.Lock()
-	defer c.mu.Unlock()
-	return c.service.DryRunMode(ctx, dryRun)
+func (s *ComposeService) DryRunMode(ctx context.Context, dryRun bool) (context.Context, error) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	if dryRun {
+		cli, err := command.NewDockerCli()
+		if err != nil {
+			return ctx, err
+		}
+
+		options := flags.NewClientOptions()
+		options.Context = s.cli.CurrentContext()
+		err = cli.Initialize(
+			options,
+			command.WithInitializeClient(func(cli *command.DockerCli) (client.APIClient, error) {
+				return api.NewDryRunClient(s.cli.Client(), s.cli)
+			}),
+		)
+		if err != nil {
+			return ctx, err
+		}
+
+		s.dryRun = true
+		s.cli = cli
+		s.overrideOutputStreams()
+		s.service = api.NewServiceProxy().WithService(compose.NewComposeService(s.cli))
+	}
+	return context.WithValue(ctx, api.DryRunKey{}, dryRun), nil
 }
